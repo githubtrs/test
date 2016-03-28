6603f6a75744e53caa8ecaa31037e237287fa00a
trs_sk <260439218@qq.com>
2016-03-28 16:41:42 +0800

    s

--- src/com/test/hello_girl.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class hello_girl {
-     public static void main(String[] args) {
-		System.out.println("hello girl");
-		System.out.println("hello boy");
-	}
-}
--- src/wulala/zwsj.java
+++ src/wulala/zwsj.java
@@ -3,5 +3,5 @@ package wulala;
 public class zwsj {
    public static void main(String[] args) {
 	System.out.println(1);
-   }
+   }	
 }
